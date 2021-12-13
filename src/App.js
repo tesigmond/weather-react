ec4652243f7b9e9b7129a5ba5b6855bf01433fb6
tesigmond <tesigmond@gmail.com>
2021-12-13 15:47:27 -0800

    Added link to github repository

--- src/App.js
+++ src/App.js
@@ -8,6 +8,10 @@ export default function App() {
         <Search />
         <Weather />
       </header>
+      <p>
+        This website is coded by Tory Sigmond and is{" "}
+        <a href="https://github.com/tesigmond/weather-react">open-sourced</a>
+      </p>
     </div>
   );
 }
